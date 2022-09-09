aa7f0792cc15fbd535ec5e5db87ec5e33635e16d
AtuaheneBarimah <110355559+AtuaheneBarimah@users.noreply.github.com>
2022-09-09 19:18:06 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,29 +7,29 @@
  */
 int main(void)
 {
-  int c, i, k;
+	int c, i, k;
 
-  for (c = '0'; c <= '9'; c++)
-    {
-      for (i = '0'; i <= '9'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-	  for (k = '0'; k <= '9'; k++)
-	    {
-	      if (c < i && i < k)
+		for (i = '0'; i <= '9'; i++)
 		{
-		  putchar(c);
-		  putchar(i);
-		  putchar(k);
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (c < i && i < k)
+				{
+					putchar(c);
+					putchar(i);
+					putchar(k);
 
-		  if (c != '7')
-		    {
-		      putchar(',');
-		      putchar(' ');
-		    }
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
 		}
-	    }
 	}
-    }
-  putchar('\n');
-  return (0);
+	putchar('\n');
+	return (0);
 }
